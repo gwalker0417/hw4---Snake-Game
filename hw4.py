0298e641318a6e243519247f8858f62f4542a9df
gwalker0417 <94934880+gwalker0417@users.noreply.github.com>
2021-11-23 15:47:24 -0600

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,147 @@
+import argparse	
+import os
+import sys
+import pygame
+import time
+import random
+	
+####
+pygame.init()
+	
+####
+white = (255,255,255)
+black = (0,0 ,0)
+blue = (55,118,171)
+green = (0,255,0) 
+gray = (128, 128, 128)
+	
+dis_width = 800
+dis_height = 600
+	
+snake_block = 10
+snake_speed = 10
+	
+######
+	
+dis=pygame.display.set_mode((dis_width,dis_height))
+# pygame.display.update()
+pygame.display.set_caption('snake')
+clock = pygame.time.Clock()
+	
+font_style = pygame.font.SysFont(None,25)
+score_font = pygame.font.SysFont(None,35)
+	
+def your_score(score):
+	value = score_font.render("Score:" + str(score), True, gray)
+	dis.blit(value, [0, 0])
+	
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+	
+def message(msg,color):
+    mesg = font_style.render(msg,True,color)
+    dis.blit(mesg, [dis_width/2, dis_height/2])
+	
+def game_loop():
+	    game_over = False
+	    game_close = False
+	
+	    x1 = dis_width/2
+	    y1 = dis_height/2
+	
+	    x1_change = 0
+	    y1_change = 0
+	
+	    snake_list = []
+	    length_of_snake = 1
+	
+	    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+	    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+	
+	    while not game_over:
+	
+	        while game_close == True:
+	            dis.fill(white)
+	            message("You Lost, Press Q (quit) or C (cont.)", blue)
+	            pygame.display.update()
+	
+	            for event in pygame.event.get():
+	                if event.type == pygame.KEYDOWN:
+	                    if event.key == pygame.K_q:
+	                        game_over = True
+	                        game_close = False
+	                    if event.key == pygame.K_c:
+	                        game_loop()
+	
+	        for event in pygame.event.get():
+	            print(event) # demo only
+	            if event.type == pygame.QUIT:
+	                game_over = True
+	            if event.type == pygame.KEYDOWN:
+	                if event.key == pygame.K_LEFT:
+	                    x1_change=-snake_block
+	                    y1_change=0
+	                if event.key == pygame.K_RIGHT:
+	                    x1_change=snake_block
+	                    y1_change=0
+	                if event.key == pygame.K_UP:
+	                    x1_change=0
+	                    y1_change=-snake_block
+	                if event.key == pygame.K_DOWN:
+	                    x1_change=0
+	                    y1_change=snake_block
+	        
+	        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+	            game_close = True
+	        
+	        x1 += x1_change
+	        y1 += y1_change
+	        dis.fill(green)
+	        pygame.draw.rect(dis,white,[foodx,foody,snake_block,snake_block])
+	        snake_head = []
+	
+	        snake_head.append(x1)
+	        snake_head.append(y1)
+	        snake_list.append(snake_head)
+	
+	        if len(snake_list) > length_of_snake:
+	            del snake_list[0]
+	
+	        for x in snake_list[:-1]:
+	            if x == snake_head:
+	                game_close = True
+	
+	        our_snake(snake_block, snake_list)
+	
+	        your_score(length_of_snake - 1)
+	
+	
+	        # pygame.draw.rect(dis,black,[x1,y1,snake_block,snake_block])
+	        pygame.display.update()
+	
+	        if x1 == foodx and y1 == foody:
+	            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+	            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+	            length_of_snake += 1
+	
+	            # snake_speed = snake_speed + 10
+	
+	
+	        clock.tick(snake_speed)
+	
+	    pygame.quit()
+	    quit()
+	
+game_loop()
+	
+	# message("You Lost",blue)
+	# pygame.display.update()
+	# time.sleep(2)
+
+	####
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument ('--id', type=str, default='anonymous', help='id')
+	parser.add_argument ('--file', type=str, default='file.txt', help='file')
+	args = parser.parse_args()
